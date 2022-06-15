94cdc52a34c051f6c0b5bf7184c396ec49f2015f
Luis Molero <luis.molero@outlook.com>
2022-06-14 21:47:25 -0300

    APP Agenda - Edita e Salva em tempo de execução

--- app/src/main/java/br/com/molero/agenda/ui/activity/FormularioAlunoActivity.java
+++ app/src/main/java/br/com/molero/agenda/ui/activity/FormularioAlunoActivity.java
@@ -1,11 +1,15 @@
 package br.com.molero.agenda.ui.activity;
 
+import static br.com.molero.agenda.ui.activity.ConstantesActivities.CHAVE_ALUNO;
+import static br.com.molero.agenda.ui.activity.ConstantesActivities.TITULO_APPBAR_EDITA_ALUNO;
+import static br.com.molero.agenda.ui.activity.ConstantesActivities.TITULO_APPBAR_NOVO_ALUNO;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import br.com.molero.agenda.R;
@@ -14,53 +18,72 @@ import br.com.molero.agenda.model.Aluno;
 
 public class FormularioAlunoActivity extends AppCompatActivity {
 
-    public static final String TITULO_APPBAR = "Novo aluno";
     private EditText campoNome;
     private EditText campoTelefone;
     private EditText campoEmail;
     private final AlunoDAO dao = new AlunoDAO();
+    private Aluno aluno;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_aluno);
-        setTitle(TITULO_APPBAR);
-        inicializaçãoDosCampos();
+        inicializacaoDosCampos();
         configuraBotaoSalvar();
+        carregaAluno();
+    }
+
+    private void carregaAluno() {
+        Intent dados = getIntent();
+        if (dados.hasExtra(CHAVE_ALUNO)) {
+            setTitle(TITULO_APPBAR_EDITA_ALUNO);
+            aluno = (Aluno) ((Intent) dados).getSerializableExtra(CHAVE_ALUNO);
+            preencheCampos();
+        } else {
+            setTitle(TITULO_APPBAR_NOVO_ALUNO);
+            aluno = new Aluno();
+        }
+    }
+
+    private void preencheCampos() {
+        campoNome.setText(aluno.getNome());
+        campoTelefone.setText(aluno.getTelefone());
+        campoEmail.setText(aluno.getEmail());
     }
 
     private void configuraBotaoSalvar() {
         Button botaoSalvar = findViewById(R.id.activity_formulario_aluno_botao_salvar);
-
         botaoSalvar.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View v) {
-
-                Aluno alunoCriado = criaAluno();
-                salva(alunoCriado);
+            @Override
+            public void onClick(View view) {
+                finalizaFormulario();
             }
         });
     }
 
-    private void inicializaçãoDosCampos() {
+    private void finalizaFormulario() {
+        preencheAluno();
+        if (aluno.temIdValido()) {
+            dao.edita(aluno);
+        } else {
+            dao.salva(aluno);
+        }
+        finish();
+    }
+
+    private void inicializacaoDosCampos() {
         campoNome = findViewById(R.id.activity_formulario_aluno_nome);
         campoTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
         campoEmail = findViewById(R.id.activity_formulario_aluno_email);
     }
 
-    private void salva(Aluno alunoCriado) {
-        dao.salva(alunoCriado);
-        finish();
-    }
-
-    @NonNull
-    private Aluno criaAluno() {
+    private void preencheAluno() {
         String nome = campoNome.getText().toString();
         String telefone = campoTelefone.getText().toString();
         String email = campoEmail.getText().toString();
-
-        Aluno alunoCriado = new Aluno(nome, telefone, email);
-        return alunoCriado;
+        aluno.setNome(nome);
+        aluno.setTelefone(telefone);
+        aluno.setEmail(email);
     }
 }
--- app/src/main/java/br/com/molero/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/br/com/molero/agenda/ui/activity/ListaAlunosActivity.java
@@ -1,8 +1,11 @@
 package br.com.molero.agenda.ui.activity;
 
+import static br.com.molero.agenda.ui.activity.ConstantesActivities.TITULO_APPBAR;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -11,22 +14,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.List;
+
 import br.com.molero.agenda.R;
 import br.com.molero.agenda.dao.AlunoDAO;
+import br.com.molero.agenda.model.Aluno;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
-    public static final String TITULO_APPBAR = "Lista de alunos";
     private final AlunoDAO dao = new AlunoDAO();
+    private ArrayAdapter<Aluno> adapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
         setTitle(TITULO_APPBAR);
-        confifuraFabNovoAluno();
+        configuraFabNovoAluno();
+        dao.salva(new Aluno("Luis", "1122223333", "luis@gmail.com.br"));
+        dao.salva(new Aluno("Karen", "1122223333", "karen@gmail.com"));
     }
 
+    private void configuraFabNovoAluno() {
+        FloatingActionButton botaoNovoAluno = findViewById(R.id.activity_main_fab_novo_aluno);
+        botaoNovoAluno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abreFormularioModoInsereAluno();
+            }
+        });
+    }
+
+    private void abreFormularioModoInsereAluno() {
+        startActivity(new Intent(this, FormularioAlunoActivity.class));
+    }
 
     @Override
     protected void onResume() {
@@ -34,29 +55,29 @@ public class ListaAlunosActivity extends AppCompatActivity {
         configuraLista();
     }
 
-    private void confifuraFabNovoAluno() {
-        FloatingActionButton fabNovoAluno = findViewById(R.id.activity_main_fab_novo_aluno);
+    private void configuraLista() {
+        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        final List<Aluno> alunos = dao.todos();
+        configuraAdapter(listaDeAlunos, alunos);
+        configuraListenerDeCliquePorItem(listaDeAlunos);
+    }
 
-        fabNovoAluno.setOnClickListener(new View.OnClickListener() {
+    private void configuraListenerDeCliquePorItem(ListView listaDeAlunos) {
+        listaDeAlunos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
-                abreFormularioAlunoACtivity();
+            public void onItemClick(AdapterView<?> adapterView, View view, int posicao, long id) {
+                Aluno aluno = (Aluno) adapterView.getItemAtPosition(posicao);
+                Intent vaiParaFormularioActivity = new Intent(ListaAlunosActivity.this, FormularioAlunoActivity.class);
+                vaiParaFormularioActivity.putExtra("aluno", aluno);
+                startActivity(vaiParaFormularioActivity);
             }
         });
     }
 
-    private void abreFormularioAlunoACtivity() {
-        startActivity(new Intent(this, FormularioAlunoActivity.class));
-    }
-
-    private void configuraLista() {
-        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
-
+    private void configuraAdapter(ListView listaDeAlunos, List<Aluno> alunos) {
         listaDeAlunos.setAdapter(new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_list_item_1,
-                dao.todos()));
+                alunos));
     }
-
-
 }
